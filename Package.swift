2738f739348d3530a2d5c158e12ee2dca263b127
skqew <57089009+skqew@users.noreply.github.com>
2019-10-29 13:53:42 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "MyLibrary",
+    name: "SlideMenuController",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "MyLibrary",
-            targets: ["MyLibrary"]),
+            name: "SlideMenuController",
+            targets: ["SlideMenuController"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,11 +19,11 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "MyLibrary",
+            name: "SlideMenuController",
             dependencies: [],
             path: "Sources"),
         .testTarget(
-            name: "MyLibraryTests",
-            dependencies: ["MyLibrary"]),
+            name: "SlideMenuControllerTests",
+            dependencies: ["SlideMenuController"]),
     ]
 )
