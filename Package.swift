fb8c1ad7f41e632f3241cdbeb7c0acdde1be6aed
skqew <57089009+skqew@users.noreply.github.com>
2019-10-29 13:59:35 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "SlideMenuController",
+    name: "SlideMenuControllerSwift",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "SlideMenuController",
-            targets: ["SlideMenuController"]),
+            name: "SlideMenuControllerSwift",
+            targets: ["SlideMenuControllerSwift"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,11 +19,11 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "SlideMenuController",
+            name: "SlideMenuControllerSwift",
             dependencies: [],
-            path: "SlideMenuControllerSwift"),
+            path: "Sources"),
         .testTarget(
-            name: "SlideMenuControllerTests",
-            dependencies: ["SlideMenuController"]),
+            name: "SlideMenuControllerSwiftTests",
+            dependencies: ["SlideMenuControllerSwift"]),
     ]
 )
